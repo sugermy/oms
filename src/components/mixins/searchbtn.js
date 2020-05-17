28622261a9976a38df752fa1a730d65826c71b47
sugermy <sugermy.willup@qq.com>
2020-05-17 15:10:40 +0800

    页面内获取按钮

--- src/components/mixins/searchbtn.js
+++ src/components/mixins/searchbtn.js
@@ -11,7 +11,21 @@ export default {
     getSearchBtns () {
       const Token = omsStorage.get('access_token')
       this.$ajax.post(`/login/menubtn/${Token}`, { menu_path: this.htmlEncode(this.$route.path) }).then(res => {
-        console.log(res)
+        const resData = res.Data || []
+        const newArr = []
+        for (let i = 0; i < resData.length; i++) {
+          resData[i].Children = []
+          if (resData[i].ParentID === 0) {
+            newArr.push(resData[i])
+          }
+          for (let k = i + 1; k < resData.length; k++) {
+            if (resData[i].ButtonID === resData[k].ParentID) {
+              resData[i].Children.push(resData[k])
+            }
+          }
+        }
+
+        this.searchOptions.buttonlist = newArr
       })
     }
   }
