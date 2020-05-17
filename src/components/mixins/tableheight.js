819267c1c4f049d9c1efb296662a16fbf8f4c4a2
sugermy <sugermy.willup@qq.com>
2020-05-17 15:10:55 +0800

    表格高度确认

--- src/components/mixins/tableheight.js
+++ src/components/mixins/tableheight.js
@@ -8,7 +8,7 @@ export default {
     this.$nextTick(() => {
       // 获取元素高度
       var h = document.documentElement.clientHeight || document.body.clientHeight
-      this.tableHeight = h - this.$refs.searchbar.$el.offsetHeight - 160
+      this.tableHeight = h - this.$refs.searchbar.$el.offsetHeight - 180
     })
   }
 }
